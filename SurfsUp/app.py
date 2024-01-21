bba24b62e8f17ea24b97638e9020a32473a328e3
Esi-Akotia <esi.akotia@gmail.com>
2024-01-21 13:40:21 -0500

    building API

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,15 +1,24 @@
 # Import the dependencies.
+import os
 from flask import Flask, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import and_
+import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# Get the absolute path to the directory containing this script
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the SQLite database file
+database_path = os.path.join(current_directory, "Resources", "hawaii.sqlite")
+
+# Create the engine with the absolute path
+engine = create_engine(f"sqlite:///{database_path}")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -34,7 +43,6 @@ last_12_months_data = session.query(Measurement.date, Measurement.prcp)\
 precipitation_results = {result.date: result.prcp for result in last_12_months_data}
 
 # List of stations  
-# QQ - station ID or station NAME?! ----------------------------------------------------- 
 all_stations = session.query(Station.name).all()
 all_stations_list = [station[0] for station in all_stations]
 
@@ -43,7 +51,7 @@ last_12_months_temp_data = session.query(Measurement.date, Measurement.tobs)\
     .filter(and_(Measurement.date >= year_before, Measurement.date <= most_recent_date))\
     .filter(Measurement.station == 'USC00519281')\
     .order_by(Measurement.tobs).all()
-temperatures = [temp[0] for temp in last_12_months_temp_data]
+temperatures = [temp[1] for temp in last_12_months_temp_data]
 
 #################################################
 # Flask Setup
@@ -76,6 +84,47 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temperature_obs():
     return jsonify (temperatures)
+
+@app.route("/api/v1.0/<start>")
+def specified_start(start):
+    
+    # Convert the start parameter to a datetime object
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    
+    # Query the minimum, maximum, and average temperature for dates greater than or equal to the start date
+    start_values = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs))\
+    .filter(Measurement.date >= start_date).all()
+    
+     # Extract the values from the query result
+    min_temp, max_temp, avg_temp = start_values[0]
+
+    # Create a dictionary to hold the results
+    start_results = {
+        "min_temperature": min_temp,
+        "max_temperature": max_temp,
+        "avg_temperature": avg_temp
+        }
+    return jsonify (start_results)
+
+@app.route("/api/v1.0/<start>/<end>")
+def specified_range(start, end):
+    # Convert the start and end parameters to datetime objects
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+
+    range_values = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs))\
+    .filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
+
+    # Extract the values from the query result
+    min_temp, max_temp, avg_temp = range_values[0]
+
+    # Create a dictionary to hold the results
+    range_results = {
+        "min_temperature": min_temp,
+        "max_temperature": max_temp,
+        "avg_temperature": avg_temp
+    }
+    return jsonify (range_results)
 
 if __name__ == "__main__":
     app.run(debug=True)
