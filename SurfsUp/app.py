6f96056ce7efcc18694cfc2c3d761b634cdd9748
Esi-Akotia <esi.akotia@gmail.com>
2024-01-20 22:35:03 -0500

    app queries

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,62 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from dateutil.relativedelta import relativedelta
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
+# Convert precipitation query results to dictionary
+most_recent_date = session.query(func.max(Measurement.date)).scalar()
+most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+year_before = most_recent_date - relativedelta(years=1)
+last_12_months_data = session.query(Measurement.date, Measurement.prcp)\
+    .filter(and_(Measurement.date >= year_before, Measurement.date <= most_recent_date))\
+    .all()
+precipitation_results = {result.date: result.prcp for result in last_12_months_data}
+
+# List of stations   
+ 
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def homepage():
+    return (
+        f"Welcome to Esi's Climate App!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+        )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return jsonify (precipitation_results)
